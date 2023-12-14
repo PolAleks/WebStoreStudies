759aa1a56975a15a8ffed20ede3cec2cba7a9a7c
AleksHome <aa.polushkin@yandex.ru>
2023-12-14 15:46:52 +0300

    Добавили модель данных

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static List<Employees> _employees = new List<Employees>
+            {
+                new Employees {Id = 1, FirstName = "Иван", SurName = "Иванов", Patronymic = "Иванович", Age = 39},
+                new Employees {Id = 2, FirstName = "Петр", SurName = "Петров", Patronymic = "Петрович", Age = 20},
+                new Employees {Id = 3, FirstName = "Сидор", SurName = "Сидоров", Patronymic = "Сидорович", Age = 45},
+            };
+
         public IActionResult Index()
         {
             return View();
@@ -12,6 +21,11 @@ namespace WebStore.Controllers
         public IActionResult SomeAction()
         {
             return View();
+        }
+
+        public IActionResult Employees()
+        {
+            return View(_employees);
         }
     }
 }
